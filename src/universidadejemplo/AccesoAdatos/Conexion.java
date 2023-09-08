5cabc325d6aa09880a02827322196132ada27e34
Matias <matiasmatteoni@hotmail.com>
2023-09-08 19:58:04 -0300

    conexion java
    Codigo de conexion

--- src/universidadejemplo/AccesoAdatos/Conexion.java
+++ src/universidadejemplo/AccesoAdatos/Conexion.java
@@ -5,10 +5,46 @@
  */
 package universidadejemplo.AccesoAdatos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author PC1
  */
 public class Conexion {
-    
+
+    private static final String URL = "jdbc:mariadb://localhost/";
+    private static final String DB = "universidadulp";
+    private static final String USUARIO = "root";
+    private static String PASSWORD = "";
+
+    private static Connection connection;
+
+    //Metodo Constructor
+    private Conexion() {
+    }
+
+    public static Connection getConexion() {
+
+        if (connection == null) {
+
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                //Setup the connection with the DB
+                connection = DriverManager
+                        .getConnection(URL + DB + "?useLegacyDatetimeCode=false&serverTimezone=UTC"
+                                + "&user=" + USUARIO + "&password=" + PASSWORD);
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error al conectarse a la BD " + ex.getMessage());
+            } catch (ClassNotFoundException ex) {
+
+                JOptionPane.showMessageDialog(null, "Error al cargar los Drivers " + ex.getMessage());
+            }
+        }
+        return connection;
+        }
+
 }
