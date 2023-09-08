2c95058445a8fc9921ac2c80f2899c2f6f8a3b53
gimenezdiegoruben <gimenezdiegoruben@gmail.com>
2023-09-08 18:29:07 -0300

    ObjetosEstructura
    agregar estructura

--- src/universidadejemplo/Entidades/Materia.java
+++ src/universidadejemplo/Entidades/Materia.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package universidadejemplo.Entidades;
 
 /**
@@ -10,5 +5,50 @@ package universidadejemplo.Entidades;
  * @author PC1
  */
 public class Materia {
+
+    private int idMateria;
+    private String nombre;
+    private int anioMateria;
+    private boolean activo;
+
+    public Materia() {
+    }
+
+    public Materia(int idMateria, String nombre, int anioMateria, boolean activo) {
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.anioMateria = anioMateria;
+        this.activo = activo;
+    }
+
+    public Materia(String nombre, int anioMateria, boolean activo) {
+        this.nombre = nombre;
+        this.anioMateria = anioMateria;
+        this.activo = activo;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnioMateria() {
+        return anioMateria;
+    }
+
+    public void setAnioMateria(int anioMateria) {
+        this.anioMateria = anioMateria;
+    }
     
 }
