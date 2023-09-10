bc91ef19347cfefed18d43bd0f67f335c9f2f845
louisizentesano <louisizentesano@gmail.com>
2023-09-10 20:40:19 -0300

    Trabajando controlador CargaNotas

---
+++ src/universidadejemplo/Controlador/ControladorCargaNotas.java
@@ -0,0 +1,40 @@
+
+package universidadejemplo.Controlador;
+
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//import java.sql.Connection;
+//import universidadejemplo.AccesoAdatos.AlumnoData;
+//import universidadejemplo.AccesoAdatos.MateriaData;
+//import universidadejemplo.Entidades.Alumno;
+//import universidadejemplo.Vistas.CargaNotas;
+//import universidadejemplo.Vistas.GestionMateria;
+//import universidadejemplo.Vistas.MenuPrincipal;
+
+/**
+  * @author louisinette
+ */
+//public class ControladorCargaNotas {
+//    public class ControladorCargaNotas implements ActionListener{
+//    private Connection con;
+//    private final CargaNotas vista;
+//    private final AlumnoData data;
+//    private final MenuPrincipal menu;
+//    
+//        public ControladorCargaNotas(CargaNotas vista, AlumnoData data, MenuPrincipal menu) {
+//        this.vista = vista;
+//        this.data = data;
+//        this.menu = menu;
+//        vista.jbtBuscar.addActionListener(this);
+//        vista.jbtSalir.addActionListener(this);
+//        vista.jbtNuevo.addActionListener(this);
+//        vista.jbtEliminar.addActionListener(this);
+//        vista.jbtGuardar.addActionListener(this);
+//        
+//    }
+//    
+//    
+//    
+//    
+//    
+//}
