e392892c27d05e14fa5a640c8f95824aa2cbb8a7
Matias <matiasmatteoni@hotmail.com>
2023-09-12 22:01:18 -0300

    otro update

--- src/universidadejemplo/Controlador/ControladorGestionAlumnos.java
+++ src/universidadejemplo/Controlador/ControladorGestionAlumnos.java
@@ -21,14 +21,14 @@ import universidadejemplo.Vistas.MenuPrincipal;
  *
  * @author Matias
  */
-public class ControladorGestionAlumnos implements ActionListener{
+public class ControladorGestionAlumnos implements ActionListener {
+
     private Connection con;
     private final GestionAlumnos vista;
     private final AlumnoData data;
     private final MenuPrincipal menu;
 
-    
-    public ControladorGestionAlumnos(GestionAlumnos vista, AlumnoData data, MenuPrincipal menu){
+    public ControladorGestionAlumnos(GestionAlumnos vista, AlumnoData data, MenuPrincipal menu) {
         this.vista = vista;
         this.data = data;
         this.menu = menu;
@@ -39,36 +39,42 @@ public class ControladorGestionAlumnos implements ActionListener{
         vista.jbtGuardar.addActionListener(this);
     }
 
-    public void iniciar(){
-    
+    public void iniciar() {
+
         menu.jFondo.removeAll();
         menu.jFondo.repaint();
         menu.jFondo.add(vista);
         vista.setVisible(true);
         menu.jFondo.moveToFront(vista);
         vista.requestFocus();
-        
+
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent ae) {
-        
-        if (ae.getSource() == vista.jbtBuscar){
-           Alumno a = new Alumno();
-           a = data.buscarAlumnoPorDni(Integer.parseInt(vista.jtxDocumento.getText()));
-           if (a != null){
-               vista.jtxDocumento.setText(a.getIdAlumno()+ "");
-               vista.jtxNombre.setText(a.getNombre());
-               vista.jtxApellido.setText(a.getApellido());
-           }else{
-               JOptionPane.showMessageDialog(null, "Su Consulta no pudo ser llevada a cabo");
-           }
+        if (!vista.jtxDocumento.getText().isEmpty()) {
+            try {
+                int dni = Integer.parseInt(vista.jtxDocumento.getText());
+                Alumno a = data.buscarAlumnoPorDni(dni);
+                if (a != null) {
+                    vista.jtxDocumento.setText(Integer.toString(a.getIdAlumno()));
+                    vista.jtxNombre.setText(a.getNombre());
+                    vista.jtxApellido.setText(a.getApellido());
+                } else {
+                    JOptionPane.showMessageDialog(null, "No se encontró al alumno.");
+                }
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "El número de documento debe ser un valor numérico válido.");
+            }
+        } else {
+            // En caso de que este vacio
+            JOptionPane.showMessageDialog(null, "Ingrese un número de documento válido.");
         }
-        
-        if (ae.getSource() == vista.jbtSalir){
+
+        if (ae.getSource() == vista.jbtSalir) {
             vista.dispose();
         }
-        if(ae.getSource() == vista.jbtNuevo){
+        if (ae.getSource() == vista.jbtNuevo) {
             vista.jbtNuevo.setEnabled(false);
             vista.jbtEliminar.setEnabled(false);
             vista.jtxDocumento.setText("-1");
@@ -78,29 +84,28 @@ public class ControladorGestionAlumnos implements ActionListener{
             vista.jtxNombre.requestFocus();
             //Si alguno sabe cual es la mejor opcion de codigo para Jcalendar, puede editar aqui sin problema.
         }
-       if (ae.getSource() == vista.jbtGuardar) {
-        vista.jbtNuevo.setEnabled(true);
-        vista.jbtEliminar.setEnabled(true);
-        vista.jtxDocumento.setEnabled(true);
-        
-        int dni = Integer.parseInt(vista.jtxDocumento.getText());
-        String nombre = vista.jtxNombre.getText();
-        String apellido = vista.jtxApellido.getText();
-        boolean estado = true; // Asumiendo que siempre quieres establecer el estado en verdadero
-        
-        
-        if (dni != -1 && dni != 1) {
-            // Código para modificar el alumno existente
-            Alumno a = new Alumno(dni, apellido, nombre, LocalDate.MIN, estado);
-            data.modificarAlumno(a);
-        } else {
-            // Código para guardar un nuevo alumno
-            Alumno b = new Alumno(dni, apellido, nombre, LocalDate.MIN, true);
-            data.guardarAlumno(b);
+        if (ae.getSource() == vista.jbtGuardar) {
+            vista.jbtNuevo.setEnabled(true);
+            vista.jbtEliminar.setEnabled(true);
+            vista.jtxDocumento.setEnabled(true);
+
+            int dni = Integer.parseInt(vista.jtxDocumento.getText());
+            String nombre = vista.jtxNombre.getText();
+            String apellido = vista.jtxApellido.getText();
+            boolean estado = true; // Asumiendo que siempre quieres establecer el estado en verdadero
+            
+            if (dni != -1 && dni != 1) {
+                // Código para modificar el alumno existente
+                Alumno a = new Alumno(dni, apellido, nombre, LocalDate.MIN, estado);
+                data.modificarAlumno(a);
+            } else {
+                // Código para guardar un nuevo alumno
+                Alumno b = new Alumno(dni, apellido, nombre, LocalDate.MIN, true);
+                data.guardarAlumno(b);
+            }
         }
     }
-    }
-    
+
     public void focusGained(FocusEvent e) {
         try {
             if (e.getSource() == vista.jtxDocumento) {
@@ -142,4 +147,5 @@ public class ControladorGestionAlumnos implements ActionListener{
             ex.printStackTrace(); // Esto es solo un ejemplo, puedes hacer algo más adecuado en tu aplicación.
         }
     }
+    
 }
