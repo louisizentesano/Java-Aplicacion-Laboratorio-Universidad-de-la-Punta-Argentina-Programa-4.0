8482f6c323c3a4c788437595f081a09b3163bcdf
Matias <matiasmatteoni@hotmail.com>
2023-09-15 19:17:06 -0300

    gg

--- src/universidadejemplo/Controlador/ControladorGestionAlumnos.java
+++ src/universidadejemplo/Controlador/ControladorGestionAlumnos.java
@@ -128,20 +128,24 @@ public class ControladorGestionAlumnos implements ActionListener {
             if (idAlumno == -1) {
                 // Pregunta al usuario si quiere guardar el nuevo alumno
                 int confirmacion = JOptionPane.showConfirmDialog(null, "¿Está seguro de guardar el nuevo alumno?", "Confirmación", JOptionPane.YES_NO_OPTION);
-            }
-                // Convierte el Instant a LocalDate utilizando una zona horaria específica
-                LocalDate fecha = instant.atZone(ZoneId.systemDefault()).toLocalDate();
-                if (dni > 0) {
-                    // Código para modificar el alumno existente
-                    Alumno a = new Alumno(dni, apellido, nombre, fecha, estado);
-                    data.guardarAlumno(a);
-                } else {
-                    // Código para guardar un nuevo alumno
-                    Alumno b = new Alumno(dni, apellido, nombre, fecha, true);
-                    data.guardarAlumno(b);
+
+                if (confirmacion == JOptionPane.YES_OPTION) {
+                    // Convierte el Instant a LocalDate utilizando una zona horaria específica
+                    LocalDate fecha = instant.atZone(ZoneId.systemDefault()).toLocalDate();
+                    if (dni > 0) {
+                        // Código para modificar el alumno existente
+                        Alumno a = new Alumno(dni, apellido, nombre, fecha, estado);
+                        data.guardarAlumno(a);
+                    } else {
+                        // Código para guardar un nuevo alumno
+                        Alumno b = new Alumno(dni, apellido, nombre, fecha, true);
+                        data.modificarAlumno(b);
+                    }
                 }
             }
         }
+    }
+    
 
     
 
