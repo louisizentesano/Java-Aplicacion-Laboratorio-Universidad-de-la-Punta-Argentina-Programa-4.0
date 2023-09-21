85bec43a4d228c4baa3a277e5a422a4ac5f1b245
gimenezdiegoruben <gimenezdiegoruben@gmail.com>
2023-09-21 12:05:49 -0300

    Update controlador Inscripciones
    Quité algunas variables y métodos no utilizados. Solo falta terminar botón anular inscripción que quite a materia inscripta y la pase a las materias no inscriptas.

--- src/universidadejemplo/Controlador/ControladorInscripciones.java
+++ src/universidadejemplo/Controlador/ControladorInscripciones.java
@@ -62,41 +62,30 @@ public class ControladorInscripciones implements ActionListener, ListSelectionLi
         vista.jRadioButtonMateriasInscriptas.setSelected(true); //muestra por defecto materias inscriptas inicializada
         rellenarTabla(); //rellena la tabla de materias incriptas inicializada
         vista.jbtInscribir.setEnabled(false); //desactiva el boton Inscribir (ya que muestra por defecto materias inscriptas)
-       }
+    }
 
     @Override
-    //acciones cuando cada boton se activa
-    public void actionPerformed(ActionEvent e) {
 
-        if (e.getSource() == vista.jComboBListAlum) {
+    public void actionPerformed(ActionEvent e) {                        //escucha acciones cuando cada boton se activa
+
+        if (e.getSource() == vista.jComboBListAlum) {                   //rellena el combo box con los alumnos
             rellenarTabla();
         }
 
-        if (e.getSource() == vista.jRadioButtonMateriasInscriptas) {
-            // Deshabilita el botón "Inscribir" cuando se selecciona "Materias Inscriptas"
-            vista.jbtInscribir.setEnabled(false);
-            vista.jRadioButtonMateriasNoInscriptas.setSelected(false);
-
-            // habilita el botón "Anular Inscripción" cuando se selecciona "Materias No Inscriptas"
-            vista.jbtAnularInscripcion.setEnabled(true);
-            vista.jRadioButtonMateriasInscriptas.setSelected(true);
-
+        if (e.getSource() == vista.jRadioButtonMateriasInscriptas) {    // Deshabilita el botón "Inscribir" cuando se selecciona "Materias Inscriptas" y deja habilitado boton "Anular Inscripción"
+            vista.jbtInscribir.setEnabled(false);                       // dehabilita el botón "Inscripción" 
+            vista.jbtAnularInscripcion.setEnabled(true);                // habilita el botón "Anular Inscripción" 
             rellenarTabla();
         }
 
         if (e.getSource() == vista.jRadioButtonMateriasNoInscriptas) {
-            // Habilita el botón "Inscribir" cuando se selecciona "Materias No Inscriptas"
-            vista.jbtInscribir.setEnabled(true);
-            vista.jRadioButtonMateriasInscriptas.setSelected(false);
-
-            // Deshabilita el botón "Anular Inscripción" cuando se selecciona "Materias No Inscriptas"
-            vista.jbtAnularInscripcion.setEnabled(false);
-            vista.jRadioButtonMateriasNoInscriptas.setSelected(true);
-
+            // Habilita el botón "Inscribir" cuando se selecciona "Materias No Inscriptas" y deshabilita boton "Anular Inscripción"
+            vista.jbtInscribir.setEnabled(true);                         // Habilita el botón "Inscribir"
+            vista.jbtAnularInscripcion.setEnabled(false);                // Deshabilita el botón "Anular Inscripción" 
             rellenarTabla();
         }
 
-        if (e.getSource() == vista.jbtInscribir) {  //Buscar incripcion el alumno a incribir en 
+        if (e.getSource() == vista.jbtInscribir) {                      //Buscar incripcion el alumno a incribir en 
 
             if (vista.jTable1.getSelectedRow() != -1) {
 
@@ -104,7 +93,7 @@ public class ControladorInscripciones implements ActionListener, ListSelectionLi
                 Materia materiasNueva = new Materia();
                 Alumno alumn = new Alumno();
 
-                int filaSelect = vista.jTable1.getSelectedRow(); //obtener fila seleccionada
+                int filaSelect = vista.jTable1.getSelectedRow();        //obtener fila seleccionada
                 int idMateriaSelect = (int) modelo.getValueAt(filaSelect, 0);
 
                 materiasNueva.setIdMateria(idMateriaSelect);
@@ -114,7 +103,7 @@ public class ControladorInscripciones implements ActionListener, ListSelectionLi
                 inscribir.setMateria(materiasNueva);
                 inscribir.setNota(0);
 
-                inscripcionData.guardarInscripcion(inscribir); //al alumno lo isncribe en esa materia
+                inscripcionData.guardarInscripcion(inscribir);          //inscribe al alumno en esa materia usando método guardarInscripcion de inscripcionData
                 rellenarTabla();
             } else {
                 JOptionPane.showMessageDialog(null, "El alumno ya está inscrito en esta materia.");
@@ -123,13 +112,12 @@ public class ControladorInscripciones implements ActionListener, ListSelectionLi
             //  JOptionPane.showMessageDialog(null, "No se pudo incribir al alumno");
         }
 
-        if (e.getSource() == vista.jbtSalir) { // sale del jInternalFrame
+        if (e.getSource() == vista.jbtSalir) { // sale del jInternalFrame al presionar botón salir
             vista.dispose();
         }
 
         if (e.getSource() == vista.jbtAnularInscripcion) {
             anularInscripcionAlumno();
-            //            JOptionPane.showMessageDialog(null, "no se pudo anular la incripción");
         }
 
         if (e.getSource() == vista.jRadioButtonMateriasInscriptas) {
@@ -147,69 +135,15 @@ public class ControladorInscripciones implements ActionListener, ListSelectionLi
     public void valueChanged(ListSelectionEvent e) {
     }
 
-    private void cargarMateriasNoCursadas() {
-        // Obtener todas las materias disp.
-        materiasDisponibles = inscripcionData.obtenerMateriasNoCursadas(alumnoSeleccionado.getIdAlumno());
-        actualizarTablaConMaterias(materiasDisponibles);
-
-        //      List<Materia> materiasDisponibles = inscripcionData.obtenerMateriasNoCursadas(alumnosSeleccionado.getIdAlumno());
-        // Cargar las materias no inscritas en la tabla
-        modelo.setRowCount(0);// Limpia la tabla
+    public void modelaTabla() {
         modelo.addColumn("ID");
         modelo.addColumn("Nombre");
-        modelo.addColumn("Año");
+        modelo.addColumn("año");
 
-        vista.jTable1.setModel(modelo);
-
+        vista.jTable1.setModel(modelo);//seteando 
     }
 
-    private void cargarMateriasCursadas() {
-// Obtener las materias en las que el alumno está inscrito
-
-        materiasInscriptas = inscripcionData.obtenerMateriasCursadas(alumnoSeleccionado.getIdAlumno());
-        actualizarTablaConMaterias(materiasInscriptas);
-    }
-
-    private void actualizarTablaConMaterias(List<Materia> materias) {
-        DefaultTableModel modelo = (DefaultTableModel) vista.jTable1.getModel();
-        modelo.setRowCount(0);
-
-        for (Materia materia : materias) {
-            modelo.addRow(new Object[]{materia.getIdMateria(), materia.getNombre(), materia.getAnio()});
-        }
-    }
-
-    private void rellenarTabla() {
-
-        List<Materia> materias = new ArrayList<Materia>(); //array list vacio para cargar los datos que provienen del metodo inscripcion data obtener materias cursadas
-
-        if (vista.jRadioButtonMateriasInscriptas.isSelected()) {
-            materias = inscripcionData.obtenerMateriasCursadas(traerID());
-        } else {
-            materias = inscripcionData.obtenerMateriasNoCursadas(traerID());
-        }
-
-        System.out.println(
-                "Rellenar tabla funciona. Alumnos: " + materias.size());
-
-        modelo.setRowCount(0); //borra el modelo
-
-        for (Materia materia : materias) {
-            modelo.addRow(new Object[]{materia.getIdMateria(), materia.getNombre(), materia.getAnioMateria()});//rellena el modelo
-        }
-
-        vista.jTable1.setModel(modelo); //asignamos el modelo a la vista
-
-    }
-
-    private int traerID() {
-        String varTemp = vista.jComboBListAlum.getSelectedItem().toString(); //trae el texto que está en el combo
-        String[] partes = varTemp.split("-"); // particiona la cadena usando el guón como punto de partida y genera el vector
-        int idAlumno = Integer.parseInt(partes[0].trim()); // selecciona la primer parte de l vector indice 0, lo extrae el dato que está en caracteres y lo parsea a entero
-        return idAlumno;
-    }
-
-    private void rellenarCombo() {
+    private void rellenarCombo() {      //rellena el combobox 
         List<Alumno> alumnos = new ArrayList<>();
         //o List<Alumno> materias = new ArrayList<Alumno>(); cualquiera funciona
 
@@ -225,19 +159,68 @@ public class ControladorInscripciones implements ActionListener, ListSelectionLi
         }
     }
 
-    public void modelaTabla() {
-        modelo.addColumn("ID");
-        modelo.addColumn("Nombre");
-        modelo.addColumn("año");
+    private void cargarMateriasCursadas() {// Obtiene las materias en las que el alumno está inscrito
 
-        vista.jTable1.setModel(modelo);//seteando 
+        materiasInscriptas = inscripcionData.obtenerMateriasCursadas(alumnoSeleccionado.getIdAlumno());
+        actualizarTablaConMaterias(materiasInscriptas);
     }
 
+    private void cargarMateriasNoCursadas() {
+        // Obtener todas las materias disp.
+        materiasDisponibles = inscripcionData.obtenerMateriasNoCursadas(alumnoSeleccionado.getIdAlumno());    // Cargar las materias no inscritas en la tabla
+        actualizarTablaConMaterias(materiasDisponibles);
+
+        //List<Materia> materiasDisponibles = inscripcionData.obtenerMateriasNoCursadas(alumnoSeleccionado.getIdAlumno());
+        modelo.setRowCount(0);// Limpia la tabla
+        vista.jTable1.setModel(modelo);
+    }
+
+    private void actualizarTablaConMaterias(List<Materia> materias) {
+        DefaultTableModel modelo = (DefaultTableModel) vista.jTable1.getModel();
+        modelo.setRowCount(0);
+
+        for (Materia materia : materias) {
+            modelo.addRow(new Object[]{materia.getIdMateria(), materia.getNombre(), materia.getAnio()});
+        }
+    }
+
+    private void rellenarTabla() {
+
+        List<Materia> materias = new ArrayList<Materia>(); //array list vacio para cargar los datos que provienen del metodo inscripcion data obtener materias cursadas o no cursadas segun radio buttom
+
+        if (vista.jRadioButtonMateriasInscriptas.isSelected()) {
+            materias = inscripcionData.obtenerMateriasCursadas(traerID());
+        } else {
+            materias = inscripcionData.obtenerMateriasNoCursadas(traerID());
+        }
+        //System.out.println("Rellenar tabla funciona. Alumnos: " + materias.size()); //opcional de control
+        modelo.setRowCount(0); //borra el modelo
+
+        for (Materia materia : materias) {
+            modelo.addRow(new Object[]{materia.getIdMateria(), materia.getNombre(), materia.getAnioMateria()});//rellena el modelo
+        }
+
+        vista.jTable1.setModel(modelo); //asignamos el modelo a la vista
+    }
+
+    private int traerID() {
+        String varTemp = vista.jComboBListAlum.getSelectedItem().toString(); //trae el texto que está en el combo
+        String[] partes = varTemp.split("-"); // particiona la cadena usando el guón como punto de partida y genera el vector
+        int idAlumno = Integer.parseInt(partes[0].trim()); // selecciona la primer parte de l vector indice 0, lo extrae el dato que está en caracteres y lo parsea a entero
+        return idAlumno;
+    }
+
+    private Materia obtenerMateriaSeleccionada(int fila) { //obtiene la materia selaccionada de la fila seleccionada
+        int idMateria = (int) vista.jTable1.getValueAt(fila, 0);
+        // return universidadejemplo.AccesoAdatos.IncripcionData.obtenerMateriaPorID(idMateria);
+        return new Materia();
+    }
+    
     private void inscribirAlumnoEnMateria() {
         int filaSeleccionada = vista.jTable1.getSelectedRow();
         if (filaSeleccionada != -1) {
             Materia materiaSeleccionada = obtenerMateriaSeleccionada(filaSeleccionada);
-
+            JOptionPane.showMessageDialog(null, "Inscripción anulada con éxito.");
             if (materiaSeleccionada != null) {
                 // inscripcionData.inscribirAlumnoEnMateria(alumnoSeleccionado.getId(), materiaSeleccionada.getIdMateria());
 
@@ -248,24 +231,18 @@ public class ControladorInscripciones implements ActionListener, ListSelectionLi
     private void anularInscripcionAlumno() {
         int filaSeleccionada = vista.jTable1.getSelectedRow();
         if (filaSeleccionada != -1) {
+
             Materia materiaSeleccionada = obtenerMateriaSeleccionada(filaSeleccionada);
+            // inscripcionData.borrarInscripcionMateriaAlumno(traerID(), obtenerMateriaSeleccionada(filaSeleccionada);
+
             if (materiaSeleccionada != null) {
                 int idAlumno = traerID();
                 int idMateria = materiaSeleccionada.getIdMateria();
 
                 inscripcionData.borrarInscripcionMateriaAlumno(idAlumno, idMateria);
 
-                cargarMateriasNoCursadas();
-                cargarMateriasCursadas();
                 JOptionPane.showMessageDialog(null, "Inscripción anulada con éxito.");
             }
         }
-    }
-
-    private Materia obtenerMateriaSeleccionada(int fila) {
-        int idMateria = (int) vista.jTable1.getValueAt(fila, 0);
-        // return universidadejemplo.AccesoAdatos.IncripcionData.obtenerMateriaPorID(idMateria);
-        return new Materia();
-
     }
 }
